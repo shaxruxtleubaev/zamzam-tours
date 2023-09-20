b6bf675398cee79b709c44ec31a077428264a64f
shaxruxtleubaev <tleubaevshaxrux@outlook.com>
2023-09-20 20:18:45 +0500

    update ser.py for image urls

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -5,21 +5,48 @@ from apps.videos.models import Video
 
 class ClientSerializer(ModelSerializer):
 
+    image_url = SerializerMethodField()
+
     class Meta:
 
         model = Client
         fields = '__all__'
 
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image:
+            image_url = obj.image.url
+            return request.build_absolute_uri(image_url)
+        return None
+
 class TourSerializer(ModelSerializer):
+
+    image_url = SerializerMethodField()
 
     class Meta:
 
         model = Tour
         fields = '__all__'
 
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image:
+            image_url = obj.image.url
+            return request.build_absolute_uri(image_url)
+        return None
+
 class VideoSerializer(ModelSerializer):
+
+    image_url = SerializerMethodField()
 
     class Meta:
 
         model = Video
         fields = '__all__'
+
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image:
+            image_url = obj.image.url
+            return request.build_absolute_uri(image_url)
+        return None
